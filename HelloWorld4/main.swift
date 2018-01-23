635e6067be54534cf78ab821524c257aa3bb1b3b
Itte Forsberg <itteforsberg@gmail.com>
2018-01-23 15:04:19 +0100

    First commit.

---
+++ HelloWorld4/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  HelloWorld4
+//
+//  Created by Pål Knudsen on 23.01.2018.
+//  Copyright © 2018 Pål Knudsen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+print("Detta er fett!")
+
